da0149f8d75ce9a5d8ef2f4d8ecd0e3b3fa21cb1
Shihabur <shihaburr1@gmail.com>
2020-09-02 19:49:30 +0100

    Used Express with Bodyparser on node js, needs css & ajax for no refresh

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
-/*
-Task: Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
-The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
-For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+const express = require('express');
+const bodyparser = require('body-parser');
+const port = 3000;
+const app = express();
 
-sumFibs(1) should return a number.
-sumFibs(1000) should return 1785.
-sumFibs(4000000) should return 4613732.
-sumFibs(4) should return 5.
-sumFibs(75024) should return 60696.
-sumFibs(75025) should return 135721.
-*/
+app.use(bodyparser.urlencoded({ extended: false}));
 
-var http = require('http');
-var fs = requre('fs');
-
-http.createServer((req,res) => {
-  fs.readFile('index.html', ())
-  res.writeHead(200, {'Content-Type': 'text/html'});
+app.get('/', (req,res) => {
+  //__dirname specifies absolute location
+  res.sendFile('index.html', {root: __dirname})
 })
 
-let value = 10;
-
-function sumFibs(value) {
+app.post('/calculate', (req,res) => {
+  let value = req.body.input;
+  let seq = {};
   let sum = 0;
-  let fib = 1;
+  let fib = 0;
   let prev = 1;
   for(var i = 1; i <= value; i++) {
     fib = fib + prev;
     if(fib % 2 > 0 && fib < value) {sum += fib;}
     prev = fib - prev;
   }
-  return sum;
-}
-console.log(sumFibs(10));
+  //console.log(sum);
+  res.write(`<h1>The Sum of Odd Fibonacci Values up to ${value} is ${sum}</h1>`);
+  res.end();
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
+})
